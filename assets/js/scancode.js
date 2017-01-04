8b161149af8419a24d56b75424793701d4cc0be2
Jillian <jdaguil@nexb.com>
2017-01-04 14:07:38 -0600

    #28: Made the API URL an input for product component import.

--- assets/js/scancode.js
+++ assets/js/scancode.js
@@ -535,7 +535,7 @@ $(document).ready(function () {
         // Get product name and version
         var productNameVersion = $('#product-name').val()
             .concat(':', $('#product-version').val());
-        var apiUrl = "https://enterprise.dejacode.com/api/v2/product_components/";
+        var apiUrl = $('#apiURLDejaCode').val();
         var apiKey = $('#apiKey').val();
         uploadComponents( apiUrl, createdComponents, apiKey, productNameVersion );
         $('#componentExportModal').modal('hide');
