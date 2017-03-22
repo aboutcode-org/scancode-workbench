5a1d49408371a868fa8bc488945ba269c790449b
John M. Horan <johnmhoran@gmail.com>
2017-03-22 11:53:38 -0700

    Display jsTree nodes with proper icons and in proper order

--- assets/js/scancode.js
+++ assets/js/scancode.js
@@ -268,14 +268,25 @@ $(document).ready(function () {
 
     var jstree = $('#jstree').jstree({
         "types": {
-            "folder": {
+            "directory": {
                 "icon": "glyphicon glyphicon-folder-close"
             },
             "file": {
                 "icon": "glyphicon glyphicon-file"
             }
         },
-        "plugins": [ "types"]
+        "plugins": [ "types","sort"],
+        'sort': function (a, b) {
+            a1 = this.get_node(a);
+            b1 = this.get_node(b);
+            if (a1.type == b1.type) {
+                return (a1.text.toLowerCase() < b1.text.toLowerCase()) ? -1 : 1; // ternary operator: places same-type nodes in alphabetical order
+            }
+            else {
+                return (a1.type === 'directory') ? -1 : 1; // ternary operator: places directory nodes above file nodes
+            }
+        }
+
     })
         .on('open_node.jstree', function (evt, data) {
             data.instance.set_icon(
