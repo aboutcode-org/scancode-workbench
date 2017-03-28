1a989817e89b1126a6945388f3ab9e682f50106f
Jillian <jdaguil@nexb.com>
2017-03-27 17:00:58 -0700

    #39: Finished migrating all the classes to ES6

--- assets/js/componentDataTables.js
+++ assets/js/componentDataTables.js
@@ -14,112 +14,111 @@
  #
  */
 
-function ComponentDataTable(tableID, aboutCodeDB) {
-    this.aboutCodeDB = aboutCodeDB;
-    this.dataTable = this._createDataTable(tableID);
-    this.dataTable.buttons().container().attr({
-        "id": "show-components",
-        "data-toggle": "modal",
-        "data-placement": "right",
-        "title": "Upload Components to DejaCode",
-        "data-target":"#componentExportModal"
-    });
-}
-
-module.exports = ComponentDataTable;
-
-ComponentDataTable.prototype = {
-    database: function(aboutCodeDB) {
+class ComponentDataTable {
+    constructor(tableID, aboutCodeDB) {
         this.aboutCodeDB = aboutCodeDB;
-    },
-    reload: function() {
-        var that = this;
+        this.dataTable = this._createDataTable(tableID);
+        this.dataTable.buttons().container().attr({
+            "id": "show-components",
+            "data-toggle": "modal",
+            "data-placement": "right",
+            "title": "Upload Components to DejaCode",
+            "data-target": "#componentExportModal"
+        });
+    }
+
+    database(aboutCodeDB) {
+        this.aboutCodeDB = aboutCodeDB;
+    }
+
+    reload() {
         this.aboutCodeDB.findAllComponents({})
-            .then(function(components) {
-                that.dataTable.clear();
-                that.dataTable.rows.add(components);
-                that.dataTable.draw();
+            .then((components) => {
+                this.dataTable.clear();
+                this.dataTable.rows.add(components);
+                this.dataTable.draw();
             });
-    },
-    _createDataTable: function(tableID){
-        return $(tableID).DataTable( {
+    }
+
+    _createDataTable(tableID) {
+        return $(tableID).DataTable({
             columns: ComponentDataTable.COLUMNS,
-            dom:
-            // Needed to keep datatables buttons and search inline
-                "<'row'<'col-sm-9'B><'col-sm-3'f>>" +
-                    "<'row'<'col-sm-12'tr>>" +
-                    "<'row'<'col-sm-5'i><'col-sm-7'p>>",
-            buttons: [
-                {
-                    name: "uploadDeja",
-                    text: '<i class=" fa fa-cloud-upload"></i> Upload Components'
-                }
-            ],
+            dom: // Needed to keep datatables buttons and search inline
+            "<'row'<'col-sm-9'B><'col-sm-3'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+            buttons: [{
+                name: "uploadDeja",
+                text: '<i class=" fa fa-cloud-upload"></i> Upload Components'
+            }],
             "language": {
-              "emptyTable": "No Components created."
+                "emptyTable": "No Components created."
             }
         });
     }
+
+    static get COLUMNS() {
+        return [
+            {
+                data: "review_status",
+                title: "Status",
+                name: "status"
+            },
+            {
+                data: "path",
+                title: "Path",
+                name: "path"
+            },
+            {
+                data: "name",
+                title: "Name",
+                name: "name"
+            },
+            {
+                data: "version",
+                title: "Version",
+                name: "version"
+            },
+            {
+                data: "owner",
+                title: "Owner",
+                name: "owner",
+                defaultContent: ""
+            },
+            {
+                data: "licenses[<hr/>].short_name",
+                title: "License",
+                name: "license_expression",
+                defaultContent: ""
+            },
+            {
+                "data": "copyrights[<hr/>].statements[]",
+                "title": "Copyright",
+                "name": "copyright_statements"
+            },
+            {
+                data: "programming_language",
+                title: "Programming Language",
+                name: "programming_language",
+                defaultContent: ""
+            },
+            {
+                "data": "homepage_url",
+                "title": "Homepage URL",
+                "name": "homepage_url",
+                "defaultContent": "",
+                "render": (href) => {
+                    `<a href='${href}' target="_blank"> ${href}</a>`
+                }
+            },
+            {
+                data: "notes",
+                title: "Notes",
+                name: "notes",
+                defaultContent: ""
+            }
+        ];
+    }
 }
 
-ComponentDataTable.COLUMNS = [
-    {
-        data: "review_status",
-        title: "Status",
-        name: "status"
-    },
-    {
-        data: "path",
-        title: "Path",
-        name: "path"
-    },
-    {
-        data: "name",
-        title: "Name",
-        name: "name"
-    },
-    {
-        data: "version",
-        title: "Version",
-        name: "version"
-    },
-    {
-        data: "owner",
-        title: "Owner",
-        name: "owner",
-        defaultContent: ""
-    },
-    {
-        data: "licenses[<hr/>].short_name",
-        title: "License",
-        name: "license_expression",
-        defaultContent: ""
-    },
-    {
-        "data": "copyrights[<hr/>].statements[]",
-        "title": "Copyright Statements",
-        "name": "copyright_statements"
-    },
-    {
-        data: "programming_language",
-        title: "Programming Language",
-        name: "programming_language",
-        defaultContent: ""
-    },
-    {
-        "data": "homepage_url",
-        "title": "Homepage URL",
-        "name": "homepage_url",
-        "defaultContent": "",
-        "render": function ( data, type, full, meta ) {
-            return '<a href="'+data+'" target="_blank">'+data+'</a>';
-        }
-    },
-    {
-        data: "notes",
-        title: "Notes",
-        name: "notes",
-        defaultContent: ""
-    }
-];
-
+module.exports = ComponentDataTable;
