5b96186e9d59417fdbcc635e363f618b7721f40c
John M. Horan <johnmhoran@gmail.com>
2017-09-08 09:46:12 -0700

    Display fewer columns on load #3
      * Uses "columnDefs.visible" and an array of display indices in the
        "target" property.
    
    Signed-off-by: John M. Horan <johnmhoran@gmail.com>

--- assets/js/aboutCodeDataTables.js
+++ assets/js/aboutCodeDataTables.js
@@ -159,6 +159,16 @@ class AboutCodeDataTable {
             "ajax": (dataTablesInput, dataTablesCallback) =>
                 this._query(dataTablesInput, dataTablesCallback),
             "columns": AboutCodeDataTable.TABLE_COLUMNS,
+            "columnDefs": [
+                {
+                    "targets": [ 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 25, 26, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47 ],
+                    "visible": false
+                },
+                {
+                    "targets": [ 0, 1, 8, 23, 24, 27, 28 ],
+                    "visible": true
+                }
+            ],
             "fixedColumns": {
                 leftColumns: 1
             },
