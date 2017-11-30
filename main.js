7a182a437128bb1449edfebaba71bc2b2620f8da
John M. Horan <johnmhoran@gmail.com>
2017-11-30 15:08:05 -0800

    Set initial title in index.html #185
      * No longer necessary to set in main.js.
    
    Signed-off-by: John M. Horan <johnmhoran@gmail.com>

--- main.js
+++ main.js
@@ -14,11 +14,11 @@ function createWindow() {
         height: 800,
         icon: path.join(__dirname, '/assets/app/app-icon/png/aboutcode_512x512.png')
     });
-    mainWindow.setTitle('AboutCode Manager');
+    // mainWindow.setTitle('AboutCode Manager');
     mainWindow.loadURL('file://' + __dirname + '/index.html');
-    mainWindow.webContents.on('did-finish-load',() => {
-        mainWindow.setTitle('AboutCode Manager');
-      });
+    // mainWindow.webContents.on('did-finish-load',() => {
+    //     mainWindow.setTitle('AboutCode Manager');
+    //   });
     mainWindow.on('closed', () => mainWindow = null);
 }
 
