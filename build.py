258ceb1dc7262fa39977ec5ddcdbe4cf9a6dae17
Jillian Daguil <jdaguil@nexb.com>
2018-01-12 12:49:10 -0800

    Bump version to 2.3.0 ⬆️
    Signed-off-by: Jillian Daguil <jdaguil@nexb.com>

--- build.py
+++ build.py
@@ -27,7 +27,7 @@ import zipfile
 #######################
 APP_NAME = 'AboutCode-Manager'
 APP_BUNDLE_ID = 'com.electron.aboutcode-manager'
-VERSION = '2.2.0'
+VERSION = '2.3.0'
 ELECTRON_VERSION = '1.6.14'
 
 #######################
