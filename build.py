e9d700e8c95ffb3ab360ad79360f41ed19776b07
Steven Esser <sesser@nexb.com>
2019-03-13 17:28:48 -0700

    Bump Version
    Signed-off-by: Steven Esser <sesser@nexb.com>

--- build.py
+++ build.py
@@ -27,7 +27,7 @@ import zipfile
 #######################
 APP_NAME = 'ScanCode-Workbench'
 APP_BUNDLE_ID = 'com.electron.scancode-workbench'
-VERSION = '3.0.0b2'
+VERSION = '3.0.0b3'
 ELECTRON_VERSION = '3.1.1'
 
 #######################
