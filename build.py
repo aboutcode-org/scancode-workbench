4739097780b9c7c23d11837438950e1719d7a668
John M. Horan <johnmhoran@gmail.com>
2019-12-02 17:53:27 -0800

    Create new icons #382
    * Created .png (Linux) and .ico (Windows) but .icns for macOS not yet created.
    
    Signed-off-by: John M. Horan <johnmhoran@gmail.com>

--- build.py
+++ build.py
@@ -46,19 +46,19 @@ on_linux = on_windows = on_mac = False
 if 'linux' in sys_platform :
     PLATFORM_NAME = 'linux'
     PLATFORM = 'linux'
-    ICON = 'png/workbench_512x512.png'
+    ICON = 'png/scwb_blue_01.png'
     on_linux = True
 
 elif'win32' in sys_platform :
     PLATFORM_NAME = 'windows'
     PLATFORM = 'win32'
-    ICON = 'win/workbench_256x256.ico'
+    ICON = 'win/scwb_blue_01.ico'
     on_windows = True
 
 elif 'darwin' in sys_platform :
     PLATFORM_NAME = 'macos'
     PLATFORM = 'darwin'
-    ICON = 'mac/workbench.icns'
+    ICON = 'mac/scwb_blue_01.icns'
     on_mac = True
 
 else:
