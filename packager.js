f62d96a2d66f03befbbd4f6c0c96bc2fa2a494dd
Omkar Phansopkar <omkarphansopkar@gmail.com>
2023-12-25 15:35:42 +0530

    Fixed inconsistent package & archive names
    Signed-off-by: Omkar Phansopkar <omkarphansopkar@gmail.com>

--- packager.js
+++ packager.js
@@ -1,7 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require("fs");
+const path = require("path");
 const packager = require("electron-packager");
+const archiver = require("archiver");
 const { packagerConfig } = require("./forge.config.js");
+
+const ARCHIVE_DIR = "dist";
 
 const MetaDataFiles = [
   "apache-2.0.LICENSE",
@@ -54,7 +58,7 @@ const ignoreFilesOrExtensions = [
 
 packager({
   dir: ".",
-  out: "out", // @NOTE - If 'out' dir is changed here, change PACKAGE_DIR in archive_builder too,
+  out: "out",
   overwrite: true,
   icon: "src/assets/app-icon/icon",
   prune: true,
@@ -78,4 +82,38 @@ packager({
     fs.copyFileSync(file, `${packagePath}/${file}`)
   );
   console.log(`Packaged app at ${packagePath}`);
+
+  buildPackageArchive(packagePath[0]);
 });
+
+/** @param {string} packagePath */
+function buildPackageArchive(packagePath) {
+  // Get the base name of the package directory
+  const packageName = path.basename(packagePath);
+
+  // Determine the packaging format based on the OS
+  const isWindows = process.platform === "win32";
+  const archiveFormat = isWindows ? "zip" : "tar";
+  const archiveExtension = isWindows ? "zip" : "tar.gz";
+
+  console.log("Building release archive ...");
+
+  // Ensure that the archive destination directory exists
+  if (!fs.existsSync(ARCHIVE_DIR)) {
+    fs.mkdirSync(ARCHIVE_DIR);
+  }
+
+  // Create the archive file with the same name as the package directory
+  const archiveFileName = `${packageName}.${archiveExtension}`;
+  const archiveFilePath = path.join(ARCHIVE_DIR, archiveFileName);
+  const output = fs.createWriteStream(archiveFilePath);
+  const archive = archiver(archiveFormat, { gzip: true });
+
+  output.on("close", () => {
+    console.log(`Created release archive at ${archiveFilePath}`);
+  });
+
+  archive.pipe(output);
+  archive.directory(packagePath, false);
+  archive.finalize();
+}
